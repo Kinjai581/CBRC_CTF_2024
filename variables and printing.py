7be4c87a44ff6c53b51dcccdf315d099ea079ef7
Vejhay Prakash <125725410+Kinjai581@users.noreply.github.com>
2024-02-14 13:03:02 +1100

    Create variables and printing.py

---
+++ variables and printing.py
@@ -0,0 +1,7 @@
+fizz = "of the most"
+bar = "strings are one"
+baz = "in cyber security"
+foo = "did you know"
+buzz = "problematic datastructures"
+out = f"Hey, {foo} {bar} {fizz} {buzz} {baz}?"
+print(out)
