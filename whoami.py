ba75afbb29b990047f2bc741d6073b2063ee0d93
Vejhay Prakash <125725410+Kinjai581@users.noreply.github.com>
2024-02-28 08:05:42 +1100

    Create whoami.py

---
+++ whoami.py
@@ -0,0 +1,50 @@
+user_role_1 = input()
+user_role_2 = input()
+user_role_3 = input()
+user_role_4 = input()
+user_role_5 = input()
+
+if user_role_1 == "root":
+  print("cbrc_CTF(user_1_is_the_root_user)")
+elif user_role_1 == "power":
+  print("cbrc_CTF(user_1_is_a_power_user)")
+elif user_role_1 == "normal":
+  print("cbrc_CTF(user_1_is_a_normal_user)")
+else:
+  print("cbrc_CTF(user_1_access_denied)")
+
+if user_role_2 == "root":
+  print("cbrc_CTF(user_2_is_the_root_user)")
+elif user_role_2 == "power":
+  print("cbrc_CTF(user_2_is_a_power_user)")
+elif user_role_2 == "normal":
+  print("cbrc_CTF(user_2_is_a_normal_user)")
+else:
+  print("cbrc_CTF(user_2_access_denied)")
+
+if user_role_3 == "root":
+  print("cbrc_CTF(user_3_is_the_root_user)")
+elif user_role_3 == "power":
+  print("cbrc_CTF(user_3_is_a_power_user)")
+elif user_role_3 == "normal":
+  print("cbrc_CTF(user_3_is_a_normal_user)")
+else:
+  print("cbrc_CTF(user_3_access_denied)")
+
+if user_role_4 == "root":
+  print("cbrc_CTF(user_4_is_the_root_user)")
+elif user_role_4 == "power":
+  print("cbrc_CTF(user_4_is_a_power_user)")
+elif user_role_4 == "normal":
+  print("cbrc_CTF(user_4_is_a_normal_user)")
+else:
+  print("cbrc_CTF(user_4_access_denied)")
+
+if user_role_5 == "root":
+  print("cbrc_CTF(user_5_is_the_root_user)")
+elif user_role_5 == "power":
+  print("cbrc_CTF(user_5_is_a_power_user)")
+elif user_role_5 == "normal":
+  print("cbrc_CTF(user_5_is_a_normal_user)")
+else:
+  print("cbrc_CTF(user_5_access_denied)")
